48a3cecf9443c25628b4c8d4644e8129de17cd7d
Minh Lee <minhlee.fre.mta@gmail.com>
2018-07-19 18:22:41 +0700

    add class CourseTest

--- src/main/java/course/Course.java
+++ src/main/java/course/Course.java
@@ -2,7 +2,7 @@ package course;
 
 public class Course {
     private String name;
-    private String[] students;
+    private String[] students = new String[5];
     private int studentsCount;
 
     public Course(String name) {
@@ -20,5 +20,27 @@ public class Course {
     public void addStudent(String student) {
         students[studentsCount] = name;
         studentsCount++;
+    }
+
+
+    public int getStudentsCount() {
+        return studentsCount;
+    }
+
+    public void removeStudent(String andy) {
+        boolean found = false;
+        for (int i = 0; i < studentsCount; i++) {
+            if (students[i] == name) {
+                found = true;
+                for (int j = i + 1; j < studentsCount; j++) {
+                    students[j - 1] = students[j];
+                }
+                studentsCount--;
+                System.out.println("Student \"" + name + "\" removed!");
+            }
+        }
+        if (!found) {
+            System.out.println("Student \"" + name + "\" not existed in list, nothing removed!");
+        }
     }
 }
